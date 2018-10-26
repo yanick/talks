6fb9350483843e1b820fd0402dc0c5d1efa64f87
Yanick Champoux <yanick@babyl.dyndns.org>
2018-10-25 20:45:43 -0400

    last minute changes

--- npmhead/footer.js
+++ npmhead/footer.js
@@ -1,10 +1,10 @@
+
+options.dependencies[3].callback = function() { hljs.initHighlightingOnLoad(); };
+
 Reveal.addEventListener( 'ready', function( event ) {
     let footer = document.getElementsByTagName('footer')[0];
 
-    console.log(footer);
     let sections = document.getElementsByClassName('slides')[0] ;
 
     sections.appendChild( footer );
-
-    console.log( 'footsie', sections );
 } );
